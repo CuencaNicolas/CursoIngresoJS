35a0ee64ec67b8436c92b7831e1733583cebe708
CuencaNicolas <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-06 10:17:19 -0300

    Ejercicio 1 y 2
    Aprendimos: alert, prompt, concatenar, varuiable...

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,22 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;//este la uso
+	var edad;//este no
+	nombre=prompt("ingrese nombre","natalia natalia");
+	alert("su nombre es: "+nombre);
+
+	//para realizar apuntes 
+    /*alert(nombre)
+    alert("Franco")*/
+
+     
+
+
+
+	//var person = prompt("Please enter your name", "Harry Potter");
+
+
+
 }
 
