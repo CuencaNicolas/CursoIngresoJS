6e0052bb8f7b9ca0cf6c2121fad6f15e557533d9
CuencaNicolas <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-06 21:46:10 -0300

    ejercicio 6 incompleto

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var numero uno
+    var numero dos
+    var suma
+    
+    var numero uno=document.getElementById('numeroUno').value;
+    
+    var numero dos=document.getElementById('numeroDos').value;
+    
+    var suma=parseInt(numero uno) + parseInt(numero dos);
+    
+    alert("la suma es:"+numeroUno+numeroDos);
 
+    //codigo con error, la idea es que las dos primeras variables se puedan convertir en enteros y sumarlos entre ellos.
 }
 
