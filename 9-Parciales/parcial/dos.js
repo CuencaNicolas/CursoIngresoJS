8b6934563e99b96e7fc5249261fb5fe6959243a8
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-07 12:21:26 -0300

    ejercicio incompleto

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,11 @@
 function Mostrar()
 {
-  
+    var importe	
+    var importe final
+    importe=document.getElementById('importeFinal').value;
+
+    importe final=importe+21%;
+
+    alert("El importe final es:" + importe );
+
 }
