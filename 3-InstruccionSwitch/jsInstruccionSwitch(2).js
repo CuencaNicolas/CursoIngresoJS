a46423b48f23da0ad45f6d2fa56ae5ffb77ffa38
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-14 12:25:51 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,38 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	var mesDelAño;
+
+	mesDelAño=document.getElementById('mes').value;
+
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno.");
+			break;
+
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace Frio.");
+			break;
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+			alert("Ya pasamos el frio,ahora calor!!!.");
+			break;
+	}
+		
+	
+
+
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,7 +1,31 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	
+	var mesDelAño
+	
+	mesDelAño = document.getElementById('mes').value;
+
+	switch(mesDelAño)
+	{
+		case "Enero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+		case "Julio":
+		case "Agosto":
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+			alert("Este mes tiene 30 o mas dias");
+			break;
+
+		case "Febrero":
+			alert("Este mes no tiene mas de 29 dias");
+			break;
+	}	
 
 //alert (mesDelAño);
 	
