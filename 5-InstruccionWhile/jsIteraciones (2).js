4e990929eaf636101566efd1ee5bb814cfac583e
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-20 10:48:07 -0300

    corregido

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador=10
+
+	while (contador >0)
+	{
+		document.write("<br>"+contador);
+		contador=contador-1;
+		
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,18 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave = prompt("ingrese el número clave.");
+
+	while(clave!="utn750")
+	{
+		clave=prompt("Error, ingrese el número clave.");
+	}
+		alert("Bienvenido.");
+
+
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
