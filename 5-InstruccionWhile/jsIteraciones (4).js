22f165db1c4c6e9f7b1eef321e97132100f42038
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-20 11:41:24 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,19 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero = prompt("ingrese un número entre 0 y 9.");
 
+	numero=parseint(numero);
 
+	while(numero<0 || numero>10)
+	{
+		
+		numero=prompt("Error,Ingrese numero entre 0 y 9");
+		numero=parseint(numero);
+	}	
+
+		//falta arreglar
+
+	
+	
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,14 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+	var sexo=prompt("ingrese f ó m .");
 
+	document.getElementById('Sexo').value=sexo;
 
+	while(!(sexo && sexo "m"))
+	{
+		sexo=prompt("Ingrese f ó m.");
+	}
 
-document.getElementById('Sexo').value=sexo;
-
+	alert("correcto");
 }//FIN DE LA FUNCIÓN
