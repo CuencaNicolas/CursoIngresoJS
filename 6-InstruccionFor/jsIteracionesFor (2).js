cbefdec49e763501bd4981c83bcd7b2dea2d96d4
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-21 11:24:21 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,22 @@
 function Mostrar()
 {
+	var contador=0
 
+	for(contador;contador-10;contador++)
+	{
+		document.write("<br>"+contador);
+		if(contador==10)
+		{
+			break;
+		}
+		if(contador==5)
+		{
+			continue;
+		}
+
+		document.write("despues de continue");
+	}
+
+	
 
 }
