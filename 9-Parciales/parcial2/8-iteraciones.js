4fbba2431bf10d3faee962cd44ad7a22f853de8f
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-02-28 11:59:51 -0300

    Ejercicio incompleto

--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,23 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+// se ngresa hasta eque e nombre desea el nombre de un animal el peso y la temperatura del aguila desde validar,informar la cantidad de temperaturas inpares,el nombre del animal mas pesado,el omre del animal menos pesado,la cantidad de aimales qe viven enhabitat menores a 0 inclusive,el prmedio del peso e todos los animales,la temperatura maxima y la minima
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
+	var nombreAnimal;
+	var pesoAnimal;
+	var temperaturaAnimal;
+
+	nombre=prompt("Ingrese nombre");
 	
+	peso=prompt("Ingrese peso");
+	peso=parseInt(peso);
+	
+	temperatura=prompt("Ingrese temperatura");
+	temperatura=parseInt(temperatura);
+
+	
+
+
+
+
 }
 
